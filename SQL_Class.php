1f0dc574b49b78ac54aea6a265f0a22a185db3d4
Radek Dolewa <R.Dolewa@gmail.com>
2013-04-03 23:59:55 +0200

    @
    @

--- SQL_Class.php
+++ SQL_Class.php
@@ -1,33 +1,42 @@
 <?php 
 
 
-$conn = mysql_connect();
+// $conn = mysql_connect();
 
-mysql_select_db ('db', $conn);
-$results = mysql_querry ('Select * FROM users');
+// mysql_select_db ('db', $conn);
+// $results = mysql_querry ('Select * FROM users');
 
-while ($row = mysql_fetch_object($results)){
-	echo $row-username;
-}
+// while ($row = mysql_fetch_object($results)){
+// 	echo $row-username;
+// }
 
 
-function querry($query){
+// function querry($query){
 	
-	$results = mysql_query($query);
+// 	$results = mysql_query($query);
 	
-	if ($results){
-		$rows= array();
-		while($row = mysql_fetch_object($results){
-			$rows[] =$row;
-		}
-		return $rows;
-	}
-	return false;
-}
+// 	if ($results){
+// 		$rows= array();
+// 		while($row = mysql_fetch_object($results){
+// 			$rows[] =$row;
+// 		}
+// 		return $rows;
+// 	}
+// 	return false;
+// }
 
-$results = querry('SELECT * FROM ');
+// $results = querry('SELECT * FROM ');
 
-foreach ($results as $row){
-	echo $row->username;
-}
+// foreach ($results as $row){
+// 	echo $row->username;
+// }
+
+
+$String = "fff";
+$String2 = "aaaa";
+
+$sec = $String.$String2;
+
+echo $sec;
+
 ?>
